9b5d7063c5bedcc170c1206e251af5da05b9187f
olajideosho <olajideosho1995@gmail.com>
2019-09-05 21:17:07 +0100

    Add project files.

---
+++ CrudUsingXamarin/CrudUsingXamarin/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrudUsingXamarin.Models
+{
+    public class Company
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + "(" + this.Address + ")";
+        }
+    }
+}
